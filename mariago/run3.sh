dfd66e75493ff09fcad1e749b99c52bd1aca2a8f
Adu <96992708+NevemSzaboAdam@users.noreply.github.com>
2025-01-15 21:41:24 +0100

    Update run3.sh

--- mariago/run3.sh
+++ mariago/run3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # docker.service, docker.socket
-#docker create network mynet
+#docker network create mynet
 docker build -t mymaria:v1 -f ./Dockerfile_3_mariadb .
 docker create -it -p 3306:3306 --network mynet --hostname mariadb01 --name mariadb01 mymaria:v1
 docker start mariadb01
